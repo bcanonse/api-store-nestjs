42e43d951ec7c4438f1faf1a6c8eee32e44af7df
Bryan Canon <bcanonse@gmail.com>
2024-10-14 14:53:55 -0600

    [ADD]Use of decorator param and query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +13,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  //Todas las rutas estaticas deben de ir primero y luego colocar las rutas dinamicas para evitar colisiones con las rutas.
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `Product with id ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Category with id ${id} of producto with id: ${productId}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit: number = 100,
+    @Query('offset') offset: number = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`;
   }
 }
