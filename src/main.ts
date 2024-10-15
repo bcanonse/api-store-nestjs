fbdf63e46ddb4ce2adb4de938b250e06a4119da3
Bryan Canon <bcanonse@gmail.com>
2024-10-15 10:57:48 -0600

    [ADD]Custom validation pipe message badrequest

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message:
+            error.constraints[
+              Object.keys(error.constraints)[0]
+            ],
+        }));
+        return new BadRequestException(result);
+      },
+      stopAtFirstError: true,
       whitelist: true, // Ignorar datos que no esten en los DTO,
       forbidNonWhitelisted: true, // Lanzar error si existen datos prohibidos
       disableErrorMessages:
