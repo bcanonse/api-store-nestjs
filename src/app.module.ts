35c8571a1ccdf1fba27d732c146b56dba18a0dd6
Bryan Canon <bcanonse@gmail.com>
2024-10-16 11:47:32 -0600

    [ADD]Config type envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,14 @@ import { HttpModule, HttpService } from '@nestjs/axios';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { environments } from './environments';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath:
         environments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     HttpModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,25 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('TASKS') private readonly tasks: any[],
-    private readonly config: ConfigService,
+    // private readonly config: ConfigService,
+    @Inject(config.KEY)
+    private readonly configService: ConfigType<
+      typeof config
+    >,
   ) {}
 
   getHello(): string {
-    const apiKey = this.config.get<string>('API_KEY');
-    const db = this.config.get<string>('DATABASE_NAME');
-    return `Hello World! ${apiKey} with db: ${db}`;
+    /* const apiKey = this.config.get<string>('API_KEY');
+    const db = this.config.get<string>('DATABASE_NAME'); */
+    const apiKey = this.configService.apiKey;
+    const db = this.configService.database.name;
+    const port = this.configService.database.port;
+    return `Hello World! ${apiKey} with db: ${db} running in port ${port}`;
   }
 }
