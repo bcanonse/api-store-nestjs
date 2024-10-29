0bb3ddcd9aec0e6f542a31b2b6a683dc6a9d0693
Bryan Canon <bcanonse@gmail.com>
2024-10-29 15:28:00 -0600

    [ADD]Read value from environment for guard decorator

--- src/auth/guards/api-key/api-key.guard.ts
+++ src/auth/guards/api-key/api-key.guard.ts
@@ -2,17 +2,26 @@ import {
   CanActivate,
   ExecutionContext,
   ForbiddenException,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from 'src/auth/decorators/public.decorator';
+import config from 'src/config';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    @Inject(config.KEY)
+    private readonly configService: ConfigType<
+      typeof config
+    >,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
@@ -27,12 +36,12 @@ export class ApiKeyGuard implements CanActivate {
       .switchToHttp()
       .getRequest<Request>();
 
-    const authHeader = request.header('Auth') ?? '';
+    const authHeader = request.header('API_KEY') ?? '';
 
     if (!authHeader)
       throw new ForbiddenException(`Not header value`);
 
-    if (authHeader !== 'EWQOIEWR')
+    if (authHeader !== this.configService.apiKey)
       throw new UnauthorizedException(`Not allowed`);
 
     return true;
